b0756acb42cc18fc26827aae82a7033efddd462e
AlexandreMarcelinoSM <98421429+AlexandreMarcelinoSM@users.noreply.github.com>
2022-01-25 19:28:19 +0000

    teste
    teste

---
+++ PAP/tlimtlim.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 25-Jan-2022 às 18:45
+-- Versão do servidor: 8.0.27
+-- versão do PHP: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `tlimtlim`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `email` varchar(50) NOT NULL,
+  `nome_responsavel` varchar(100) NOT NULL,
+  `telefone` int NOT NULL,
+  `morada` varchar(150) NOT NULL,
+  `nome_paciente` varchar(100) NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `equipa`
+--
+
+DROP TABLE IF EXISTS `equipa`;
+CREATE TABLE IF NOT EXISTS `equipa` (
+  `email` varchar(50) NOT NULL,
+  `nome` varchar(40) NOT NULL,
+  `telefone` int NOT NULL,
+  `morada` varchar(150) NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `marcacoes`
+--
+
+DROP TABLE IF EXISTS `marcacoes`;
+CREATE TABLE IF NOT EXISTS `marcacoes` (
+  `email` varchar(100) NOT NULL,
+  `nome_responsavel` varchar(100) NOT NULL,
+  `telefone` int NOT NULL,
+  `morada` varchar(100) NOT NULL,
+  `data` date NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
